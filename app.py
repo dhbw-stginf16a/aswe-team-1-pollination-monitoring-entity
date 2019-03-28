6e1823bb4ceb1691c32adb040c44b0fb1f92800e
Anne Born <mail@anne-born.de>
2019-03-28 14:56:13 +0100

    rebuild registration

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ from requests.exceptions import ConnectionError
 logger = logging.getLogger(__name__)
 
 
-CENTRAL_NODE_BASE_URL = os.environ["CENTRAL_NODE_BASE_URL"]
-OUR_URL = os.environ["OWN_URL"]
+CENTRAL_NODE_BASE_URL = os.environ.setdefault("CENTRAL_NODE_BASE_URL", "http://localhost:8080/api/v1")
+OUR_URL = os.environ.setdefault("OWN_URL", "http://localhost:8081/api/v1")
 
 app = connexion.App(__name__, specification_dir='openapi/')
 app.add_api('openapi.yml')
@@ -31,7 +31,7 @@ logger.info('Updater intialized')
 
 
 # Try to register the application on app startup
-@application.before_first_request
+#@application.before_first_request
 def register():
     while True:
         logger.info("Attempt registration")
